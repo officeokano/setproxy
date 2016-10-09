ac133842c80e53a553f2d32a01b26e41749716e1
H Okano <h.okano@gmail.com>
2016-10-09 14:52:17 +0900

    modified

--- proxydisable.js
+++ proxydisable.js
@@ -1,6 +1,6 @@
 function proxydisable(){
 	var wshell = new ActiveXObject("WScript.Shell");
-	var key = "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\";
+	var key = "HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\\";
 	wshell.regwrite(key + "ProxyEnable", 0, "REG_DWORD");
 	window.alert("プロキシを解除しました");
 }
--- proxyenable.js
+++ proxyenable.js
@@ -1,6 +1,6 @@
 function proxyenable(){
 	var wshell = new ActiveXObject("WScript.Shell");
-	var key = "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\";
+	var key = "HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\\";
 	wshell.regwrite(key + "ProxyEnable", 1, "REG_DWORD");
 	window.alert("プロキシを設定しました");
 }
